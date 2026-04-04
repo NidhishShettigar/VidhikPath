75d8cd8dbe31badee0f7e1343ee3b3b5c59d1f25
Nidhish Shettigar <nidhishshettigar23@gmail.com>
2026-04-04 23:54:49 +0530

    final fix firebase + settings

--- vidhikpath/settings.py
+++ vidhikpath/settings.py
@@ -14,7 +14,9 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'your-secret-key-here'
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
